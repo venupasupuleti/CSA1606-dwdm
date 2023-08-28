4925492961c30769c077c9f4b09ad34261d44ae8
venu <114422032+venupasupuleti@users.noreply.github.com>
2023-08-29 00:18:52 +0530

    Create 23.1.Multiple regression(1).R

---
+++ 23.1.Multiple regression(1).R
@@ -0,0 +1,31 @@
+# Multiple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('data2.csv')
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+dataset$State
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+print(regressor)
+plot(regressor)
