cbc7f17ed04406a0979f635958e3beede465a6a6
venu <114422032+venupasupuleti@users.noreply.github.com>
2023-08-29 00:11:25 +0530

    Create 16.Pie chart.R

---
+++ 16.Pie chart.R
@@ -0,0 +1,5 @@
+a <- c(80,70,50,60,70,100) 
+result<-(pie(a,main="Pie Chart Marks scored in Daily Test",
+             labels=c("Nani","Sai","Saiteja","Vinay","Harsha","Mohith"), 
+             col = c("red", "orange", "yellow", "blue", "green","grey"))) 
+print(result) 
